24267e9ed3a7c5d9f3853df0cf4915cccc8732b8
Barissa-Imran <imranbarissa@gmail.com>
2023-08-31 23:37:26 +0300

    add 0x00-personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,9 @@ patterns = {
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """Filters a log line"""
     extract, replace = (patterns['exract'], patterns['replace'])
     return re.sub(extract(fields, separator), replace(redaction), message)
